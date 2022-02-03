ba99c975cc3d6a76d7b55184ba37eb89c11a394a
john gallagher <127highstreet@gmail.com>
2022-02-03 18:46:05 -0500

    ph1

---
+++ Member.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace soloHeist
+{ 
+    
+    public class Member 
+    {//Create a way to store a single team member. A team member will have a name, a skill Level and a courage factor. The skill Level will be a positive integer and the courage factor will be a decimal between 0.0 and 2.0.
+        public string name {get; set;}
+        public int skillLevel {get; set;}
+
+        public double courageFactor {get; set;}
+
+
+
+    };
+    
+
+
+}
--- Program.cs
+++ Program.cs
@@ -4,9 +4,57 @@ namespace soloHeist
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\n Let Us Plan Your Heist!");
+
+            Questions("\n Let us gather a team for the heist.  \n What is a member's name?","\nGreat!  Next, what is the member's skill level?", "\nSuper! Lastly what is the member's courage factor? Please enter from 0.00 to 2.00.");
+
+            
+
+        }
+
+        static void Questions (string question1, string question2, string question3){
+            Console.WriteLine(question1);
+
+            string typedName = Console.ReadLine();
+
+            Console.WriteLine(question2);
+
+            int typedSkillLevel = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(question3);
+
+            double typedCourageFactor = Double.Parse(Console.ReadLine ());
+
+            Member member1 = new Member {
+            name = typedName,
+            skillLevel = typedSkillLevel,
+            courageFactor = typedCourageFactor
+             };
+
+             Console.WriteLine($"\nYou have entered the member's name as \"{member1.name}\" with the skill level of \"{member1.skillLevel}\" and with the courage factor of \"{member1.courageFactor}.\" ");
+
+        }
+
+             
+
+
+             
+
+            
+
+             
+
+           
+
+
+
+
+
+
         }
     }
-}
+

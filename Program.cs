3954f091e6907a2a9d0eaadc7ddda69e3a7150e6
john gallagher <127highstreet@gmail.com>
2022-02-02 22:43:32 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace soloHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
